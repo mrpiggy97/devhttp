e825bb3f0d6a50a8ea36200e859e79807c835050
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-08-26 16:05:46 +0000

    finished placing Execute function in its own file

--- src/cmd/Command.ts
+++ src/cmd/Command.ts
@@ -164,24 +164,3 @@ export function addCommand(cmd : Command) : void{
 export function removeCommand(cmd : Command) : void{
     Cmd.delete(cmd.name)
 }
-
-export function Execute(args : string) : Log{
-    // at this point we should get something like
-    // app create --name extension --port 3000
-    let argsArray : string[] = args.split(" ")
-    if(argsArray.length < 2){
-        return {
-            message: "not enough arguments were provided",
-            error : true
-        }
-    }
-    const selectedCmd : Command | undefined = Cmd.get(argsArray[0])
-    if(!selectedCmd){
-        return {
-            message : "no such command exists",
-            error : true
-        }
-    }
-    argsArray = argsArray.splice(1)
-    return selectedCmd.Execute(argsArray)
-}
---
+++ src/cmd/Execute.ts
@@ -0,0 +1,22 @@
+import { Command, Log, Cmd } from "./Command";
+
+export function Execute(args : string) : Log{
+    // at this point we should get something like
+    // app create --name extension --port 3000
+    let argsArray : string[] = args.split(" ")
+    if(argsArray.length < 2){
+        return {
+            message: "not enough arguments were provided",
+            error : true
+        }
+    }
+    const selectedCmd : Command | undefined = Cmd.get(argsArray[0])
+    if(!selectedCmd){
+        return {
+            message : "no such command exists",
+            error : true
+        }
+    }
+    argsArray = argsArray.splice(1)
+    return selectedCmd.Execute(argsArray)
+}
