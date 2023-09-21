f549dc1f3ad367df22c5d8a5ef9bb9a447269335
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-09-21 17:48:18 +0000

    renamed file

---
+++ src/cmd/Execute.tsx
@@ -0,0 +1,35 @@
+import { Command, Cmd, Log } from "./Command";
+import Error from "./Error";
+
+export default function Execute(args : string) : Log{
+    // at this point we should get something like
+    // app create --name extension app --port 3000
+    let argsArray : string[] = args.split(" ")
+    if(argsArray.length < 2){
+        const flags = new Map<string, string | undefined>()
+        flags.set("--message", "not enough arguments were provided")
+        const optFlags = new Map<string, string | undefined>()
+        return {
+            props: {
+                flags: flags,
+                optFlags : optFlags
+            },
+            executor: Error
+        }
+    }
+    const selectedCmd : Command | undefined = Cmd.get(argsArray[0])
+    if(!selectedCmd){
+        const flags = new Map<string, string | undefined>()
+        flags.set("--message", "no such command exists")
+        const optFlags = new Map<string, string | undefined>()
+        return {
+            props: {
+                flags: flags,
+                optFlags: optFlags
+            },
+            executor: Error
+        }
+    }
+    argsArray = argsArray.splice(1)
+    return selectedCmd.Execute(argsArray)
+}
