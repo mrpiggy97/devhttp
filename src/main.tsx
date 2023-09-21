0bcb95281164adf2ff6393c3e0166859be212746
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-09-21 17:56:12 +0000

    added method to start terminal extension

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import startConsole from './cmd/cmd.ts'
+
+startConsole()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
