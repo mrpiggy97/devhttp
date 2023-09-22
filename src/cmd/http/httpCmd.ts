ef8753f679850b0027c025488b522a003a9b4953
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-09-22 05:49:33 +0000

    sub commands are now to be declared inside the parent command file

--- src/cmd/http/httpCmd.ts
+++ src/cmd/http/httpCmd.ts
@@ -1,4 +1,6 @@
-import { Command } from "../Command";
-import { getCmd } from "./GetCmd";
+import { Command, SubCommand } from "../Command";
+import GetExecutor from "./GetExecutor";
+
+const getCmd : SubCommand = new SubCommand("get", GetExecutor, ["--url", "--headers"], [], "makes a GET HTTP request")
 
 export const httpCmd : Command = new Command("http",[getCmd])
