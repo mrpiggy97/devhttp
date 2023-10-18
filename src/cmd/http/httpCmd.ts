b92a51faaaa5ce524de6719bfe65f95c4e5f1a86
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-10-18 06:06:36 +0000

    updated flags for get sub command

--- src/cmd/http/httpCmd.ts
+++ src/cmd/http/httpCmd.ts
@@ -3,7 +3,7 @@ import GetExecutor from "./GetExecutor";
 import PostExecutor from "./PostExecutor";
 import PutExecutor from "./PutExecutor";
 
-const getCmd = new SubCommand("get", GetExecutor, ["--url", "--headers"], [], "makes a GET HTTP request")
+const getCmd = new SubCommand("get", GetExecutor, ["--url"], ["--headers"], "makes a GET HTTP request")
 const postCmd = new SubCommand("post", PostExecutor, ["--url", "--token", "--data"],[],"makes a POST HTTP request")
 const putCmd = new SubCommand("put", PutExecutor, ["--url", "--token", "--data"],[], "make a PUT HTTP request")
 
