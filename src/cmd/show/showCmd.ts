a97e893895d9dbd4314828156736fe4977d5407b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-10-19 03:35:11 +0000

    finish creating command to get file size of html5 document

--- src/cmd/show/showCmd.ts
+++ src/cmd/show/showCmd.ts
@@ -1,5 +1,7 @@
 import { Command, SubCommand } from "../Command";
 import ShowIpExecutor from "./ShowIpExecutor";
+import FileSizeExecutor from "./FileSizeExecutor";
 
 const ipCmd = new SubCommand("ip", ShowIpExecutor, [],[],"show your current ip address")
-export const showCmd = new Command("show", [ipCmd])
+const fileSizeCmd = new SubCommand("file-size", FileSizeExecutor, [],[],"shows the size of the current html5 document")
+export const showCmd = new Command("show", [ipCmd, fileSizeCmd])
