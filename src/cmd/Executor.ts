c0985f6f80833728643f6b03b582dfdb7c77ccea
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-11-20 20:50:26 +0000

    flags and opt flags are not allowed to be null

--- src/cmd/Executor.ts
+++ src/cmd/Executor.ts
@@ -1,7 +1,7 @@
 
 export type ExecutorProps = {
-    flags : Map<string, string | undefined>,
-    optFlags : Map<string,string | undefined>,
+    flags : Map<string, string | undefined> | null,
+    optFlags : Map<string,string | undefined> | null,
 }
 
 export type Executor = (props : ExecutorProps) => JSX.Element
