dc7766892cba290aab6318a52ef2dc4f2830f6b4
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-11-20 21:11:59 +0000

    flags and opt flags are required

--- src/cmd/Executor.ts
+++ src/cmd/Executor.ts
@@ -1,7 +1,7 @@
 
 export type ExecutorProps = {
-    flags : Map<string, string | undefined> | null,
-    optFlags : Map<string,string | undefined> | null,
+    flags : Map<string, string | undefined>,
+    optFlags : Map<string,string | undefined>,
 }
 
 export type Executor = (props : ExecutorProps) => JSX.Element
