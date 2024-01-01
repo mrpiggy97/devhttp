0539e5da3a508e7da890a5207a5ebce431e9ef88
mrpiggy97 <fabyjesusrivas10@gmail.com>
2024-01-01 19:45:39 +0000

    fixed bug involving map keys

--- src/cmd/http/AliasExecutor.tsx
+++ src/cmd/http/AliasExecutor.tsx
@@ -1,12 +1,12 @@
 import { ExecutorProps } from "../Executor";
 
 export default function AliasExecutor(props : ExecutorProps) : JSX.Element{
-    const url : string = props.flags.get("url") as string
-    const alias : string = props.flags.get("alias") as string
-    localStorage.setItem(alias, url)
+    const url : string = props.flags.get("--url") as string
+    const value : string = props.flags.get("--value") as string
+    localStorage.setItem(value, url)
     return(
         <div>
-            <span>success now you can use {alias} as url value for {url}</span>
+            <span>success now you can use {value} as url value for {url}</span>
         </div>
     )
 }
