cd2677d1508ca23e44bbdf371ce4b6cc6a9a4952
mrpiggy97 <fabyjesusrivas10@gmail.com>
2024-01-01 19:44:46 +0000

    added 2 commands

--- src/cmd/http/httpCmd.ts
+++ src/cmd/http/httpCmd.ts
@@ -3,10 +3,14 @@ import GetExecutor from "./GetExecutor";
 import PostExecutor from "./PostExecutor";
 import PutExecutor from "./PutExecutor";
 import AliasExecutor from "./AliasExecutor";
+import ShowAliasesExecutor from "./ShowAliasesExecutor";
+import ClearAliasesExecutor from "./ClearAliasesExecutor";
 
 const getCmd = new SubCommand("get", GetExecutor, ["--url"], ["--headers"], "makes a GET HTTP request")
 const postCmd = new SubCommand("post", PostExecutor, ["--url", "--token", "--data"],[],"makes a POST HTTP request")
 const putCmd = new SubCommand("put", PutExecutor, ["--url", "--token", "--data"],[], "make a PUT HTTP request")
 const aliasCmd = new SubCommand("alias", AliasExecutor, ["--url", "--value"],[],"gives an alias to a url")
+const showAliasesCmd = new SubCommand("show-aliases", ShowAliasesExecutor, [],[], "prints aliases and their urls")
+const clearAliasesCmd = new SubCommand("clear-aliases", ClearAliasesExecutor, [], [], "clears aliases from local storage")
 
-export const httpCmd : Command = new Command("http",[getCmd, postCmd, putCmd, aliasCmd])
+export const httpCmd : Command = new Command("http",[getCmd, postCmd, putCmd, aliasCmd, showAliasesCmd, clearAliasesCmd])
