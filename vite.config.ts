095569ed1d71ef1b4ef3902742294fbf0f1a7e10
mrpiggy97 <fabyjesusrivas10@gmail.com>
2024-09-22 17:20:08 +0000

    merge with dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host: true,
+    port: 3000
+  },
+  preview:{
+    host: true,
+    port: 3000
+  },
   build:{
     rollupOptions:{
       output: {
